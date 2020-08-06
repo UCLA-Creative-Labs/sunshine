fa39ed2ed0bdd0c24c42e31f9eaf9437820fe4b1
Bryan Pan <bryanpan342@gmail.com>
2020-08-06 02:03:42 -0700

    first round of changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import App from './components/app';
 
 import './global.scss';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
