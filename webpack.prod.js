990bc7826af0c8f3ce32d22afd76de0977e43f16
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-08-05 22:27:31 -0400

    chore: add basic skeleton for React website (#18)
    Use `yarn start` to start the website as a development server running on `localhost:8080`. Use `yarn build` to generate a production build.
    
    * Written in TypeScript
    * Includes basic `package.json`
    * Uses Webpack for bundling
    * Uses `netlify.toml` for redirection. See [here](https://docs.netlify.com/routing/redirects/#syntax-for-the-netlify-configuration-file)
    
    **Note:** See #17 for conversation; had to recreate PR due to some mistakes.
    
    Resolves #11

---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: 'assets/[name].[ext]',
+                        limit: 8192
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        runtimeChunk: 'multiple',
+        usedExports: true,
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].min.css',
+            chunkFilename: '[id].[contenthash].min.css'
+        })
+    ]
+});
