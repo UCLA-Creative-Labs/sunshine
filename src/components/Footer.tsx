b5d5c32296ee497ff6af7f7af35318e9369e6618
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-09-11 21:15:47 -0400

    Added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import './styles/Footer.scss';
+
 function Footer(): React.Component {
   return (
-    <h1>I AM THE FOOTER</h1>
+    <div id='footer'>
+      <span id='footer-tag'>
+        <p>Made with ❥ by</p>
+        <h3 className='logotype'>
+          CREATIVE LABS
+        </h3>
+      </span>
+      <div id='footer-nav'>
+        <nav>
+          <a>FACEBOOK</a>
+          <a>INSTAGRAM</a>
+          <a>MEDIUM</a>
+        </nav>
+      </div>
+    </div>
   );
 }
 
