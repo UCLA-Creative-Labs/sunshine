4858dd5521467997fb44f954ea46366e816fbf1f
whgdk <lucendary@ucla.edu>
2020-11-24 03:59:23 -0500

    feat: team page formatting

--- src/components/Team/TeamCard.tsx
+++ src/components/Team/TeamCard.tsx
@@ -7,6 +7,7 @@ interface Person {
   class: number;
   roles: string[];
   image?: string;
+  link?: string;
 }
 
 interface TeamCardProps {
@@ -16,12 +17,13 @@ interface TeamCardProps {
 function TeamCard(props: TeamCardProps): JSX.Element {
   // TODO: move this and the alumni vs class of comparison to TeamPage
   const year = new Date().getFullYear();
+  const image = <img className="team-img" src={props.data.image} alt={props.data.name} />;
 
   return (
-    <div>
-      <img className="team-img" src={props.data.image} alt={props.data.name} />
-      <h4>{props.data.name}</h4>
-      <p>{props.data.class < year ? 'ALUMNI' : `CLASS OF ${year}`}</p>
+    <div className="team-card">
+      {props.data.link ? <a target="_blank" rel="noreferrer" href={props.data.link}> {image} </a> : image}
+      <h3 className="team-title">{props.data.name}</h3>
+      <p>{props.data.class < year ? 'ALUMNI' : `CLASS OF ${props.data.class}`}</p>
       {props.data.roles.map((role) => (
         <p key={props.data.name + '_' + role}>— {role}</p>
       ))}
--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select';
 
 import TeamCard from './TeamCard';
 
@@ -13,22 +14,28 @@ interface TeamProps {
   data: Person[];
 }
 
+const roles = ['Designer', 'Developer', 'Director', 'External', 'Projects'];
+const roleOptions = roles.map(item => ({ value: item.toLowerCase(), label: item }));
+
+const year = new Date().getFullYear();
+const yearOptions = [year + 1, year + 2, year + 3, year + 4, 'Alumni'].map(item => ({ value: item, label: item }));
+
 function Team(props: TeamProps): JSX.Element {
   props.data.forEach((data) => data.image && data.image.indexOf('/assets/') == -1 ? (data.image = `/assets/${data.image}`) : data.image);
 
   return (
-    <div>
+    <div className="team-page">
       <h1>THE TEAM</h1>
-      <div className="team-left">
-        <select>
-          <option>Designer</option>
-          <option>Developer</option>
-        </select>
-      </div>
-      <div className="section">
-        {props.data.map((person) => (
-          <TeamCard key={person.name} data={person} />
-        ))}
+      <div className="team-content">
+        <div className="team-left">
+          <Select className='team-select' placeholder='Year' options={yearOptions} />
+          <Select className='team-select' placeholder='Role' options={roleOptions} />
+        </div>
+        <div className="team-cards">
+          {props.data.map((person) => (
+            <TeamCard key={person.name} data={person} />
+          ))}
+        </div>
       </div>
     </div>
   );
