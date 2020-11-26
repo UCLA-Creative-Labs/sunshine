ab247ff0e526714d95467d4c4f3a4b115db3afa5
Lucy <53920397+whgdk@users.noreply.github.com>
2020-11-26 01:53:24 -0500

    feat: team page (#52)
    - team member info is in src/assets/teamInfo.json
    - name, class, and roles are required, image and link are optional
    - alumni status is automatically calculated by comparing years
    
    note: link tooltips don't show up on mobile

---
+++ src/components/Team/TeamCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
+import '../styles/Team.scss';
+
+interface Person {
+  name: string;
+  class: number;
+  roles: string[];
+  image?: string;
+  link?: string;
+}
+
+interface TeamCardProps {
+  data: Person;
+  alumni: boolean;
+}
+
+function TeamCard(props: TeamCardProps): JSX.Element {
+  const image = <img className="team-img" src={props.data.image} alt={props.data.name} />;
+
+  return (
+    <div className="team-card">
+      {props.data.link ? (
+        <>
+          <a
+            data-tip
+            data-for={props.data.name}
+            target="_blank"
+            rel="noreferrer"
+            href={props.data.link}
+          >
+            {image}
+          </a>
+          <ReactTooltip id={props.data.name} className='team-tooltip' arrowColor='transparent' textColor='black' backgroundColor='#FFF740'>
+            {props.data.link.split('//').slice(-1)[0] + ' ↗'}
+          </ReactTooltip>
+        </>
+      ) : (
+        image
+      )}
+      <h3 className="team-title">{props.data.name}</h3>
+      <p>{props.alumni ? 'ALUMNI' : `CLASS OF ${props.data.class}`}</p>
+      {props.data.roles.map((role) => (
+        <p key={props.data.name + '_' + role}>— {role}</p>
+      ))}
+    </div>
+  );
+}
+
+export default TeamCard;
