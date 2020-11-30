36dd50b025dd9d8a9f6c901ad38c53d9af9034ec
Bryan Pan <bryanpan342@gmail.com>
2020-11-30 15:35:14 -0800

    remove sheets stuff

--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -36,31 +36,5 @@ function notifications(){
   process.stdout.write('done\n');  
 }
 
-/* Write team info file */
-function teamInfo() {
-  process.stdout.write('Writing to team info file .. ');
-  const sheets = google.sheets({version: 'v4', auth: `${process.env.AUTHORIZE_SHEETS_API_KEY}`});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '11UPoREQxLhipshR4jXAGFuYfGen0WairLoxYvV9p1u4',
-    range: 'Team Info!A:E',
-  }).then((response) => formatResponse(response));
-  
-  function formatResponse(response){
-    const values = response.data.values;
-    if (!values) return;
-    const labels = values[0];
-    const team = values.slice(1).map((row) => {
-      const member = {};
-      labels.map((l, i) => {
-        member[l] = l === 'roles' ? row[i].split(', ') : row[i];
-      });
-      return member;
-    });
-    fs.writeFileSync(path.resolve(__dirname, '../src/assets/teamInfo.json'), JSON.stringify(team));
-  }
-  process.stdout.write('done\n');
-}
-
 env();
 notifications();
-teamInfo();
