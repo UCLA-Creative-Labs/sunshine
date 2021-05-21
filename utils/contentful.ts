36cdda543e52b676b95697ea3763e726cf89f469
Sriram Balachandran <sriramb2000@gmail.com>
2021-05-20 22:57:46 -0700

    feat: fetch page text from contentful

--- utils/contentful.ts
+++ utils/contentful.ts
@@ -22,6 +22,20 @@ export const query = `{
   }
 }`;
 
+export const pluginTextQuery = `
+  query sunshine_plugin_test($figmaIdPrefix: String) {
+  textEntryCollection(
+    where: {
+      figmaId_contains: $figmaIdPrefix
+    }
+  ) {
+    items {
+      figmaId
+      shortText
+    }
+  }
+}`;
+
 export const fetchTeam = async (): Promise<any[] | undefined> => {
   const res = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}/`, {
     method: 'POST',
@@ -33,4 +47,22 @@ export const fetchTeam = async (): Promise<any[] | undefined> => {
   });
   const {data} = await res.json();
   return data?.teamMembersCollection?.items ?? [];
+};
+
+export const fetchProjectTextEntries = async (figmaIdPrefix: string): Promise<any[] | undefined> => {
+  const res = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query: pluginTextQuery,
+      variables: {
+        figmaIdPrefix,
+      },
+    }),
+  });
+  const {data} = await res.json();
+  return data?.textEntryCollection?.items ?? [];
 };
