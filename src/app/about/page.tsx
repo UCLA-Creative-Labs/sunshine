2d731916eebb6e139b5caf8d46c3e41967f990d9
anderson-truong <andersonleetruong@gmail.com>
2023-10-11 13:20:20 -0700

    blank about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,10 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import HomepageSplash from "@/app/(root)/HomepageSplash";
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
-      <HomepageSplash />
       <Footer />
     </main>
   )
