870e860814fc95a848c71939e423d92ab98ac582
anderson-truong <andersonleetruong@gmail.com>
2023-10-11 01:36:27 -0700

    responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="flex flex-col md:flex-row space-y-8 md:space-y-0 justify-between items-center tracking-wide min-w-full bg-black py-10 px-20">
+        <footer className="flex flex-col md:flex-row space-y-8 md:space-y-0 justify-between items-center tracking-wide min-w-full bg-black py-10 sm:px-20">
             <div className="flex items-center">
                 <p className="text-xl md:text-xl">Made with ❤️ by &nbsp;</p> 
                 <h1 className="text-xl md:text-xl font-bold">CREATIVE LABS</h1>
