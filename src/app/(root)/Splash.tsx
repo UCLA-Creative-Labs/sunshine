b1c83f8adeda064020ff25becf0f97ccab9cf431
anderson-truong <andersonleetruong@gmail.com>
2023-10-11 01:32:31 -0700

    responsive splash text

--- src/app/(root)/Splash.tsx
+++ src/app/(root)/Splash.tsx
@@ -1,8 +1,8 @@
 export default function Splash() {
     return (
-        <div id="splash" className="flex flex-col justify-end min-h-[70vh] min-w-full bg-[#85b6ff] dark:bg-[#000000] p-10 lg:px-60 lg:py-20">
+        <div id="splash" className="flex flex-col justify-end min-h-[70vh] min-w-full bg-[#85b6ff] dark:bg-[#000000] p-10 lg:px-20 lg:py-20">
             <div>
-                <h1 id="blurb" className="text-3xl sm:text-5xl lg:text-6xl tracking-wide lg:w-[60vw]">
+                <h1 id="blurb" className="text-3xl sm:text-5xl lg:text-6xl tracking-wide lg:w-[80vw] xl:w-[60vw]">
                     A community of UCLA creatives working on cool projects to discover even cooler passions
                 </h1>
             </div>
