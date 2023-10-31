fc6c58512794b356cee6da4a6b8ef3648d27716c
elizabeth <elizabethkim03@gmail.com>
2023-10-30 18:30:06 -0700

    Update env var names in lib scripts

--- src/lib/boardMembers.js
+++ src/lib/boardMembers.js
@@ -1,37 +1,45 @@
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 // Load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
-const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
-const CONTENTFUL_API_KEY = process.env.CONTENTFUL_ACCESS_TOKEN;
+const CONTENTFUL_SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const CONTENTFUL_API_KEY = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 const CONTENTFUL_API_URL = `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}`;
 
 async function getBoardMembers() {
   try {
     const response = await axios.get(
-      `${CONTENTFUL_API_URL}/entries?content_type=teamMembers&access_token=${CONTENTFUL_API_KEY}`,
+      `${CONTENTFUL_API_URL}/entries?content_type=teamMembers&access_token=${CONTENTFUL_API_KEY}`
     );
 
-    const boardMembers = response.data.items;
+    if (response.status === 200) {
+      const boardMembers = response.data.items;
+      // console.log(boardMembers);
 
-    for (const member of boardMembers) {
-      if (member.fields.photo && member.fields.photo.sys) {
-        const assetId = member.fields.photo.sys.id;
-        try {
-          const imgResponse = await axios.get(
-            `${CONTENTFUL_API_URL}/assets/${assetId}?access_token=${CONTENTFUL_API_KEY}`,
-          );
-          member.fields.imageURL = imgResponse.data.fields.file.url;
-        } catch (error) {
-          console.error('Error fetching asset:', error);
-          throw error;
+      for (const member of boardMembers) {
+        if (member.fields.photo && member.fields.photo.sys) {
+          const assetId = member.fields.photo.sys.id;
+          try {
+            const imgResponse = await axios.get(
+              `${CONTENTFUL_API_URL}/assets/${assetId}?access_token=${CONTENTFUL_API_KEY}`,
+              { validateStatus: (status) => status >= 200 && status < 300 }
+            );
+            member.fields.imageURL = imgResponse.data.fields.file.url;
+          } catch (assetError) {
+            console.error("Error fetching asset:", assetError);
+            throw assetError;
+          }
         }
       }
+      return boardMembers;
+    } else {
+      // Handle non-2xx status code here
+      console.error("Non-2xx status code:", response.status);
+      throw new Error(`Request failed with status code ${response.status}`);
     }
-    return boardMembers;
   } catch (error) {
-    console.error('Error fetching board members:', error);
+    console.error("Error fetching board members:", error);
     throw error;
   }
 }
