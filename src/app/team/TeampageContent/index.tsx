af4bef3622a4cf8d88c87786478490515599b232
elizabeth <elizabethkim03@gmail.com>
2023-11-12 13:18:59 -0800

    responsive columns for alumni

--- src/app/team/TeampageContent/index.tsx
+++ src/app/team/TeampageContent/index.tsx
@@ -212,7 +212,7 @@ export default function TeampageContent(props: TeamProps): JSX.Element {
               aria-label="Filter by Role"
             />
           </div>
-          <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-4">
+          <div className="w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-4">
             {alumni.length
               ? alumni.map((person) => <TeamCard data={person} />)
               : "No one was found."}
