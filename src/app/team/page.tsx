53c7249622318f9801eba8e2a21f09c4026f5e32
anderson-truong <andersonleetruong@gmail.com>
2023-12-04 21:30:37 -0800

    team page with dropdown

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,32 +1,61 @@
-import DropdownMenu from "@/components/DropdownMenu";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { getDocsByType } from "@/lib/contentfulLib";
 import React, { useState, useEffect } from "react";
+import TeamContent from "./TeamContent/TeamContent";
 
+const ROLES_SORTING = [
+  "President",
+  "Director",
+  "Design",
+  "Marketing",
+  "Projects",
+  "Finance",
+  "Tech",
+  "External"
+]
 
-export default function Team() {
+function memberSort(x, y) {
+  const x_roles = x.fields?.roles;
+  const y_roles = y.fields?.roles;
+  if (!x_roles || !y_roles) return 0;
+  if (ROLES_SORTING.indexOf(x_roles[0]) < ROLES_SORTING.indexOf(y_roles[0]))
+    return -1;
+  else if (ROLES_SORTING.indexOf(x_roles[0]) > ROLES_SORTING.indexOf(y_roles[0]))
+    return 1;
+  else {
+    if (ROLES_SORTING.indexOf(x_roles[1]) != ROLES_SORTING.indexOf(y_roles[1])) {
+      if (ROLES_SORTING.indexOf(x_roles[1]) < ROLES_SORTING.indexOf(y_roles[1]))
+        return -1;
+      else if (ROLES_SORTING.indexOf(x_roles[1]) > ROLES_SORTING.indexOf(y_roles[1]))
+        return 1;
+      }
+      const x_name = x?.fields?.name;
+      const y_name = y?.fields?.name;
+    
+      if (!x_name || !y_name)
+        return 0;
+      if (x_name > y_name)
+        return 1;
+      else if (x_name < y_name)
+        return -1;
+      else
+        return 0;
+    }
+}
+
+export default async function Team() {
+  const members = await getDocsByType('member')
+    .then(docs => {
+      docs.sort(memberSort);
+      docs = docs.map(obj => ({...obj, enabled: true}));
+      return docs;
+    });
+
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
-      <div className="flex justify-center py-10 h-screen">
-        <DropdownMenu
-          className="flex flex-col w-[200px] text-xl"
-          buttonClassName="p-4 px-4 bg-white border border-gray-300 border-[1.5px] focus:border-blue-300" 
-          menuClassName="bg-white mt-1 text-center drop-shadow-md"
-          label="All Years">
-          <div className="flex flex-col">
-            <button className="py-2 hover:bg-blue-100 border border-[1.5px] border-b-0 border-gray">
-              2023
-            </button>
-            <button className="py-2 hover:bg-blue-100 border border-[1.5px] border-b-0 border-gray">
-              2022
-            </button>
-            <button className="py-2 hover:bg-blue-100 border border-[1.5px] border-gray">
-              2021
-            </button>
-          </div>
-        </DropdownMenu>
-      </div>
+      <TeamContent members={members} />
       <Footer />
     </main>
   );
--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,21 +1,28 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 
-export default function DropdownMenu({ className, buttonClassName, menuClassName, label, children } 
-    : { className?: string, buttonClassName?: string, menuClassName?: string, label?: string, children?: React.ReactNode }) {
+export default function DropdownMenu({ className, buttonClassName, menuClassName, menuButtonClassName, menuButtonHoverColor, options, setValue } 
+    : { className?: string, buttonClassName?: string, menuClassName?: string, menuButtonClassName?: string, menuButtonHoverColor?: string, options: Array<string>, setValue?: any }) {
     const [enabled, setEnabled] = useState<boolean>(false);
     const dropdownRef = useRef<any>(null);
+    const [selectedValue, setSelectedValue] = useState<string|undefined>(options?.at(0));
 
     const handleOutsideClick = (e: Event) => {
         if (dropdownRef.current && enabled && !dropdownRef.current.contains(e.target)) {
             setEnabled(false);
         }
     }
+
+    const handleEsc = (e: KeyboardEvent) => {
+        if (dropdownRef.current && enabled && e.key === "Escape")
+            setEnabled(false);
+    }
         
     useEffect(() => {
         document.addEventListener('mousedown', handleOutsideClick);
+        document.addEventListener('keydown', handleEsc);
 
         return () => {
             document.removeEventListener('mousedown', handleOutsideClick);
@@ -23,15 +30,22 @@ export default function DropdownMenu({ className, buttonClassName, menuClassName
     }, [enabled]);
 
     return (
-        <div className={className} ref={dropdownRef}>
+        <div className={`${className} relative`} ref={dropdownRef}>
             <button className={`${buttonClassName} flex items-center justify-between ${enabled ? 'rounded-t-xl rounded-b-none' : 'rounded-xl'}`} onClick={() => { setEnabled(!enabled); }}>
                 <h1>
-                    {label}
+                    {selectedValue}
                 </h1> 
-                <FaAngleDown className="mt-1"/>
+                {enabled ? <FaAngleUp className="mt-1"/> : <FaAngleDown className="mt-1"/>}
             </button>
-            <div className={`z-10 ${enabled ? 'block' : 'hidden'} ${menuClassName}`}>
-                {children}
+            <div className={`top-full flex flex-col z-10 absolute ${enabled ? 'block' : 'hidden'} ${menuClassName}`}>
+                {options.map((_, idx) => 
+                    <button 
+                        className={`${menuButtonClassName} ${options.at(idx) == selectedValue ? menuButtonHoverColor : ''}`}
+                        key={idx}
+                        onClick={() => {setSelectedValue(options.at(idx)); setEnabled(false); if (setValue) setValue(options.at(idx));}}
+                    >
+                        {options.at(idx)}
+                    </button>)}
             </div>
         </div>
     )
