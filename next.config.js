caf9d934f36b60389ac6bb0c7b6344567170e040
anderson-truong <andersonleetruong@gmail.com>
2023-12-20 12:29:17 -0800

    update homepage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // reactStrictMode: false,
+    webpack: (config) => {
+      config.externals.push({
+        "utf-8-validate": "commonjs utf-8-validate",
+        bufferutil: "commonjs bufferutil",
+        canvas: "commonjs canvas",
+      });
+      // config.infrastructureLogging = { debug: /PackFileCache/ };
+      return config;
+    },
+  };
 
 module.exports = nextConfig
