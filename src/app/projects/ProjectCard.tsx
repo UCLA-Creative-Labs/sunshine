6113b82b68b6287ed8365d8c31d8420410b96a80
Alex Zheng <75999032+smalex-z@users.noreply.github.com>
2024-10-17 00:15:30 -0700

    Basic Grid set up and CSS and structure created

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -1,12 +1,23 @@
 import { Lato } from "next/font/google"
+import Image from 'next/image';
+
+import '../../styles/ProjectsContent.scss';
+
 
 const lato = Lato({ weight: '700', subsets: ['latin'] })
 
-export default function ProjectCard({ title, description, url }) {
+export default function ProjectCard({ title, description, img, alt = "", url = "" }) {
 
     return (
-        <div className="flex flex-col">
-            <img />
+        <div className="flex flex-col proj-card">
+            <Image
+            className="project-image"
+            src={img}
+            alt={alt}
+            layout="responsive" /* Ensures the image scales based on its container */
+            width={5} /* Set the aspect ratio width */
+            height={3} /* Set the aspect ratio height */
+            />
             <h1 className="text-2xl font-bold">{title}</h1>
             <p className="text-lg">{description}</p>
         </div>
--- src/app/projects/ProjectsContent.tsx
+++ src/app/projects/ProjectsContent.tsx
@@ -1,20 +1,23 @@
 import ContentSection from "@/components/ContentSection";
 import ProjectCard from "./ProjectCard";
 
+import '../../styles/ProjectsContent.scss';
+
+
 export default function ProjectsContent() {
     return (
         <div className="grow flex flex-col space-y-10 px-20 py-20 text-black min-w-full">
+            <img src="/projects/header.png"></img>
             <ContentSection title="PROJECTS ARCHIVE">
-                <div className="flex flex-col">
-                    <h1 className="text-lg">
-                        Every quarter, we have student teams collaborate and execute an idea. Below are some projects that were created in the past quarters. Feel free to browse!
-                    </h1>
-                    <h1 className="text-lg font-bold">
-                        Currently, work in progress...
-                    </h1>
-                    {/* <div>
-                        <ProjectCard title="Fall 2023" description="Featured Projects:" url=""/>
-                    </div> */}
+                <div className="flex flex-col w-full">
+                    <div className="proj-container w-full">
+                        <ProjectCard title="Fall 2023" description="Featured Projects:" img="/projects/comingsoon.png"/>
+                        <ProjectCard title="Fall 2023" description="Featured Projects:" img="/projects/comingsoon.png"/>
+                        <ProjectCard title="Fall 2023" description="Featured Projects:" img="/projects/comingsoon.png"/>
+                        <ProjectCard title="Fall 2023" description="Featured Projects:" img="/projects/comingsoon.png"/>
+                        <ProjectCard title="Fall 2023" description="Featured Projects:" img="/projects/comingsoon.png"/>
+
+                    </div>
                 </div>
             </ContentSection>
         </div>
