e2eb9490b273859299b963a69c94c95692d867e2
smalex-z <alexzheng2004@gmail.com>
2024-10-26 06:46:06 +0000

    initial changes + css and html work, some bugs

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -1,5 +1,7 @@
 import { Lato } from "next/font/google"
 import Image from 'next/image';
+import Link from "next/link";
+
 
 import '../../styles/ProjectsContent.scss';
 
@@ -9,17 +11,19 @@ const lato = Lato({ weight: '700', subsets: ['latin'] })
 export default function ProjectCard({ title, description, img, alt = "", url = "" }) {
 
     return (
-        <div className="flex flex-col proj-card">
-            <Image
-            className="project-image"
-            src={img}
-            alt={alt}
-            layout="responsive" /* Ensures the image scales based on its container */
-            width={5} /* Set the aspect ratio width */
-            height={3} /* Set the aspect ratio height */
-            />
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <p className="text-lg">{description}</p>
-        </div>
+        <Link href={url}>
+            <div className="flex flex-col proj-card">
+                <Image
+                className="project-image"
+                src={img}
+                alt={alt}
+                layout="responsive" /* Ensures the image scales based on its container */
+                width={5} 
+                height={3} 
+                />
+                <h1 className="text-2xl font-bold">{title}</h1>
+                <p className="text-lg">{description}</p>
+            </div>
+        </Link>
     )
 }
