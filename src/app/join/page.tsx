eeb6a7cb325c7e6ddb9ce5318f3204c00420e89e
4RohanSinha <rohansinha1@yahoo.com>
2024-10-30 03:15:48 -0700

    added links

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,12 +1,28 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { getDocsByType } from "@/lib/contentfulLib";
 import JoinContent from "./JoinContent";
 
-export default function About() {
+export default async function About() {
+  let pLeadLink = "#"
+  let pMemLink = "#"
+  let bMemLink = "#"
+
+  await getDocsByType('link')
+  .then(docs => {
+	  docs.map(obj => {
+		const link: string = obj.fields.url as string;
+	  	if (obj.fields.redirectPath == "general-project-member-application") pMemLink = link;
+		else if (obj.fields.redirectPath == "general-project-lead-application") pLeadLink = link;
+		else if (obj.fields.redirectPath == "board-application") bMemLink = link;
+	  });
+  });
+
+
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
-      <JoinContent />
+      <JoinContent pLeadLink={pLeadLink} pMemLink={pMemLink} bMemLink={bMemLink} />
       <Footer />
     </main>
   )
