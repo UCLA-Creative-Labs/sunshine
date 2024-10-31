72b93e50b5620ec6d43d3d57c4cd78b12059ef81
smalex-z <alexzheng2004@gmail.com>
2024-10-31 00:08:10 +0000

    projects parsed through js db now

--- src/app/projects/ProjectsContent.tsx
+++ src/app/projects/ProjectsContent.tsx
@@ -1,5 +1,6 @@
 import ContentSection from "@/components/ContentSection";
 import ProjectCard from "./ProjectCard";
+import projectData from '@/assets/projectData';
 
 import '../../styles/ProjectsContent.scss';
 
@@ -9,44 +10,18 @@ export default function ProjectsContent() {
         <div className="grow flex flex-col space-y-10 px-20 py-20 text-black min-w-full pc">
             <img src="/projects/header.png"></img>
             <ContentSection title="">
-                <div className="flex flex-col w-full">
+                <div className="relative flex flex-col w-full">
                     <div className="proj-container w-full">
-                        {/*TODO: Can and probably should replace this with a db... i will do this later -AZ*/}
-                        <ProjectCard title="Spring 2024" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/s24"/>
-                        <ProjectCard title="Winter 2024" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/w24"/>
-                        <ProjectCard title="Fall 2023" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/f23"/>
-                        
-                        <ProjectCard title="Spring 2023" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/s23"/>
-                        <ProjectCard title="Winter 2023" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/w23"/>
-                        <ProjectCard title="Fall 2022" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/f22"/>
-                        
-                        <ProjectCard title="Spring 2022" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/s22"/>
-                        <ProjectCard title="Winter 2022" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/w22"/>
-                        <ProjectCard title="Fall 2021" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/f21"/>
-                        
-                        <ProjectCard title="Spring 2021" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/s21"/>
-                        <ProjectCard title="Winter 2021" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/w21"/>
-                        <ProjectCard title="Fall 2020" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/f20"/>
-                        
-                        <ProjectCard title="Spring 2020" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/s20"/>
-                        <ProjectCard title="Winter 2020" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/w20"/>
-                        <ProjectCard title="Fall 2019" description="Featured Projects:" 
-                        img="/projects/comingsoon.png" alt="coming soon!" url="projects/f19"/>
-
+                        {projectData.map((project, index) => (
+                            <ProjectCard
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            img={project.img}
+                            alt={project.alt}
+                            url={project.url}
+                            />
+                        ))}
                     </div>
                 </div>
             </ContentSection>
---
+++ src/assets/projectData.js
@@ -0,0 +1,40 @@
+// projectData.js
+const projectData = [
+    { title: "Spring 2024", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/s24" },
+    { title: "Winter 2024", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/w24" },
+    { title: "Fall 2023", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/f23" },
+    
+    { title: "Spring 2023", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/s23" },
+    { title: "Winter 2023", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/w23" },
+    { title: "Fall 2022", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/f22" },
+    
+    { title: "Spring 2022", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/s22" },
+    { title: "Winter 2022", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/w22" },
+    { title: "Fall 2021", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/f21" },
+    
+    { title: "Spring 2021", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/s21" },
+    { title: "Winter 2021", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/w21" },
+    { title: "Fall 2020", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/f20" },
+    
+    { title: "Spring 2020", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/s20" },
+    { title: "Winter 2020", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/w20" },
+    { title: "Fall 2019", description: "Featured Projects:", img: "/projects/comingsoon.png", 
+        alt: "coming soon!", url: "projects/f19" },
+  ];
+  
+  export default projectData;
+  
