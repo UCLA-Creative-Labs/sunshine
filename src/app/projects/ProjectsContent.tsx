c75e48bf4f4929251d0d67454b4be901a3ea5a23
smalex-z <alexzheng2004@gmail.com>
2024-10-31 01:20:36 +0000

    added img alt

--- src/app/projects/ProjectsContent.tsx
+++ src/app/projects/ProjectsContent.tsx
@@ -8,7 +8,7 @@ import '../../styles/ProjectsContent.scss';
 export default function ProjectsContent() {
     return (
         <div className="grow flex flex-col space-y-10 px-20 py-20 text-black min-w-full pc">
-            <img src="/projects/header.png"></img>
+            <img src="/projects/header.png" alt="Creative Labs Project Archive"></img>
             <ContentSection title="">
                 <div className="relative flex flex-col w-full">
                     <div className="proj-container w-full">
