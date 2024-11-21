5e4cc65af0c09732e5142a060084c8820c097a59
keyanadesai <keyana@g.ucla.edu>
2024-11-20 16:11:36 -0800

    Changed card shapes

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -4,17 +4,19 @@ const lato = Lato({ weight: '700', subsets: ['latin'] });
 
 interface ContentSectionProps {
     title?: string,
+    description?: string,
     children?: React.ReactNode,
     centerText?: boolean,
     titleClassName?: string
 }
 
-export default function ContentSection({ title, children, centerText=false, titleClassName="text-2xl md:text-4xl" } : ContentSectionProps) {
+export default function ContentSection({ title, description, children, centerText=false, titleClassName="text-2xl md:text-4xl" } : ContentSectionProps) {
     return (
-    <div className="space-y-10">
+    <div className="space-y-8">
         <h1 className={ `text-center ${!centerText && "md:text-start"} ${titleClassName} tracking-wide ${lato.className}` }>
             {title}
         </h1>
+        {description && <p className="text-center md:text-start text-lg">{description}</p>}  
         <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-16">
             { children }
         </div>
