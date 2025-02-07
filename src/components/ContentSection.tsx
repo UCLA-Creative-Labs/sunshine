ade296c0259f9ff610f20547a15a4cb1afa508a4
keyanadesai <keyana@g.ucla.edu>
2025-02-06 16:24:31 -0800

    updated the past project cards

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -15,6 +15,7 @@ export default function ContentSection({ title, description, children, centerTex
     <div className="space-y-8">
         <h1 className={ `text-center ${!centerText && "md:text-start"} ${titleClassName} tracking-wide ${lato.className}` }>
             {title}
+            <span className="ml-2 text-3xl group-hover:translate-x-2 transition-transform ease-in-out duration-300">➔</span>
         </h1>
         {description && <p className="text-center md:text-start text-lg">{description}</p>}  
         <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-16">
