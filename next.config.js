1d4fbedf451f019e924597c445123983d593aaa9
Gavin Wong <gavinmwong@gmail.com>
2025-02-24 12:08:39 -0800

    Created Year and Quarter Project Page outline

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: false,
-    webpack: (config) => {
-      config.externals.push({
-        "utf-8-validate": "commonjs utf-8-validate",
-        bufferutil: "commonjs bufferutil",
-        canvas: "commonjs canvas",
-      });
-      // config.infrastructureLogging = { debug: /PackFileCache/ };
-      return config;
-    },
+  // reactStrictMode: false,
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
 
-    env: {
-      SPACE_ID: process.env.SPACE_ID,
-      CDN_API_KEY: process.env.CDN_API_KEY,
-    },
-  };
+  env: {
+    SPACE_ID: process.env.SPACE_ID,
+    CDN_API_KEY: process.env.CDN_API_KEY,
+  },
+};
 
-  module.exports = nextConfig
+module.exports = nextConfig
