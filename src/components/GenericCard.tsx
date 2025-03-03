b2e91cf7c3be53fb8f12a90c30dcf752a7d23b4d
Justin Sheu <justinsheu3341@gmail.com>
2025-03-02 23:40:09 -0800

    Remove links from event cards (and all generic cards without specified href)

--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -13,42 +13,44 @@ interface GenericCardProps {
     href?: string
 }
 
-export default function GenericCard({ title, description, end, imgSrc='/card_icons/what-we-do.svg', imgPos='top', className='', href='/' }: GenericCardProps) {
+export default function GenericCard({ title, description, end, imgSrc='/card_icons/what-we-do.svg', imgPos='top', className='', href }: GenericCardProps) {
+    
+    const cardClasses = `flex ${
+        imgPos === "top" ? "flex-col space-y-8" : "flex-row space-x-8 items-center"
+      } group border border-black p-8 shadow-lg rounded-xl transition ease-in-out delay-50 duration-300 ${
+        href ? "cursor-pointer" : ""
+      } ${className}`;
 
-    return (
-        <Link href={href}
-            className={`flex ${imgPos == 'top' ? 'flex-col space-y-8' : 'flex-row space-x-8 items-center'} group border border-black p-8 shadow-lg rounded-xl cursor-pointer transition ease-in-out delay-50 duration-300 ${className}`}
-        >
-            <div className="overflow-hidden rounded-xl shadow-lg">
-                <img
-                    alt="Card Icon"
-                    src={imgSrc}
-                    className="w-full group-hover:scale-125 transition ease-in-out duration-500"
-                />
+    const cardContent = (
+        <>
+          <div className="overflow-hidden rounded-xl shadow-lg">
+            <img
+              alt="Card Icon"
+              src={imgSrc}
+              className="w-full group-hover:scale-125 transition ease-in-out duration-500"
+            />
+          </div>
+          <div className="space-y-6">
+            <div className="flex items-center space-x-2">
+              <h1 className={`text-3xl ${lato.className}`}>{title}</h1>
             </div>
-            <div className="space-y-6">
-                <div className="flex items-center space-x-2">
-                    <h1
-                        className={ `text-3xl ${lato.className}` }
-                    >
-                        {title}
-                    </h1>
-                </div>
-                <p
-                    className="text-xl"
-                >
-                    {description}
+            <p className="text-xl">{description}</p>
+            {end && (
+              <div className="flex items-center space-x-2">
+                <p className="text-xl relative mb-1">
+                  <span className="absolute bottom-0 left-0 w-full border-b border-black"></span>
+                  {end}
                 </p>
-                {end && (
-                    <div className="flex items-center space-x-2">
-                        <p className="text-xl relative mb-1">
-                            <span className="absolute bottom-0 left-0 w-full border-b border-black"></span>
-                            {end}
-                        </p>
-                        <h1 className={`text-xl group-hover:translate-x-4 transition ease-in-out duration-100 delay-50 ${lato.className}`}>➔</h1>
-                    </div>
-                )}
-            </div>
-        </Link>
-    )
+                <h1
+                  className={`text-xl group-hover:translate-x-4 transition ease-in-out duration-100 delay-50 ${lato.className}`}
+                >
+                  ➔
+                </h1>
+              </div>
+            )}
+          </div>
+        </>
+      );
+    
+      return href ? <Link href={href} className={cardClasses}>{cardContent}</Link> : <div className={cardClasses}>{cardContent}</div>; // if no href is passed in, do not make card clickable
 }
