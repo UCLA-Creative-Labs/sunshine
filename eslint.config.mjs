02e9913656efa6fd4a4206fab86ee7a185e6c451
Marc Jowell Bagaoisan <marcjowellgbagaoisan@gmail.com>
2026-01-08 17:26:15 -0800

    added rules for lint: no unused vars and no explicit any to be warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,8 @@ export default [
             ...nextPlugin.configs.recommended.rules,
             ...nextPlugin.configs["core-web-vitals"].rules,
             "react/no-unescaped-entities": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "warn",
         },
     },
 ];
