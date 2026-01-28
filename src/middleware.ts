02ff3cf0e083af9634d2aaa328561ba97fe1d7b6
Marc Jowell Bagaoisan <marcjowellgbagaoisan@gmail.com>
2026-01-28 12:16:35 -0800

    replaced browser client with server client since CreateBrowserCleint is client only

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@
 
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { supabase } from '@/lib/supabase/client';
+import { createMiddlewareClient } from '@/lib/supabase/server';
 
 export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
